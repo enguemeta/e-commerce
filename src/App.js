b2e658630a0c89b1bbb0c03e9f4a96739aa3321e
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2019-11-23 21:07:29 -0500

    setting up the routing ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component'
 
 function App() {
   return (
-    <div>     
+    <div>
       <HomePage />      
     </div>
   );
