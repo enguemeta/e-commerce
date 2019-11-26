1d4ccd9a5d1630c5dee0b60255e28a31f7a30175
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2019-11-25 20:07:16 -0500

    collection items, shop page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component'
+import ShopPage from './components/pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <HomePage />      
+      {/* <HomePage />*/}
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
