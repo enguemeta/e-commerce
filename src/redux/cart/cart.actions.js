333361f89917cfad564f2abc4dac2fff4aab44eb
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2019-11-30 21:12:49 -0500

    updated shop data with new data, utils, group to cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,10 +1,13 @@
 import CartActionsTypes from './cart.types';
 
-export const toggleCartHidden = () => {
-
-   // console.log(CartActionsTypes.TOGGLE_CART_HIDDEN);
+export const toggleCartHidden = () => {   
     return(
     {
     type: CartActionsTypes.TOGGLE_CART_HIDDEN
     }
-)}
+)};
+
+export const addItem = item => ({
+    type : CartActionsTypes.ADD_ITEM,
+    payload: item
+})
